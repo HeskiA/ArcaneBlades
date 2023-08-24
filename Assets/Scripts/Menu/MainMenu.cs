1e527d0c77e8d7e14bdb540782e3374fca81a107
Antonio Heski <heskiantonio@gmail.com>
2023-08-24 03:05:20 +0200

    Dodan help

--- Assets/Scripts/Menu/MainMenu.cs
+++ Assets/Scripts/Menu/MainMenu.cs
@@ -5,10 +5,11 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    public GameObject help;
     // Start is called before the first frame update
     void Start()
     {
-        
+        help.SetActive(false);
     }
 
     // Update is called once per frame
@@ -25,5 +26,13 @@ public class MainMenu : MonoBehaviour
     public void QuitGame()
     {
         Application.Quit();
+    }
+
+    public void ToggleHelp()
+    {
+        if (help.activeSelf)
+            help.SetActive (false);
+        else
+            help.SetActive(true);
     }
 }
