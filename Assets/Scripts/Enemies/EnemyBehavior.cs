9c66c6e160ef1a84c16eb6aee30c08cd89a2b6f6
DujeVidas <dujevidas123@gmail.com>
2023-08-24 13:08:48 +0200

    Dodan enemy animation

--- Assets/Scripts/Enemies/EnemyBehavior.cs
+++ Assets/Scripts/Enemies/EnemyBehavior.cs
@@ -5,16 +5,16 @@ using UnityEngine;
 public class EnemyBehavior : MonoBehaviour
 {
     private GameObject player;
-    //private PlayerMovement playerMovement;
     public float speed;
+    Animator animator;
 
     public float distanceBetween;
     private float distance;
     void Start()
-    {
+    {  
+        animator = GetComponent<Animator>();
         
         player = GameObject.Find("Player");
-        //playerMovement = player.GetComponent<PlayerMovement>();
     }
 
 
@@ -24,15 +24,29 @@ public class EnemyBehavior : MonoBehaviour
         Vector2 direction = player.transform.position - transform.forward;
         direction.Normalize();
 
-        //float angle = Mathf.Atan2(direction.y, direction.x)* Mathf.Rad2Deg;
 
         
         if(distance < distanceBetween && distance > 0.5) 
         {
-
+            Debug.Log(direction.x);
+            animator.SetBool("SeePlayer", true);
+            
             transform.position = Vector2.MoveTowards(this.transform.position, player.transform.position, speed * Time.deltaTime);
-            //transform.rotation = Quaternion.Euler(Vector3.forward*angle);
 
+            if (player.transform.position.x - this.transform.position.x > 0f)
+            {
+                
+                this.transform.localRotation = Quaternion.Euler(0, 180, 0);
+            }
+            else
+            {
+                this.transform.localRotation = Quaternion.Euler(0, 0, 0);
+            }
+
+        }
+        else
+        {
+            animator.SetBool("SeePlayer", false);
         }
     }
 
